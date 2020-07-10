962dd9b14b8655a55e1c0d5c46e4f032071fcc5d
Antonmitkov <antosha.mitkov@mail.ru>
2020-07-10 16:50:27 +0600

    first commit

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = Console.ReadLine(), a = "";
+            char B = Convert.ToChar(Console.ReadLine());
+            for (int i=0; i <A.Length;i++)
+            {
+                if (A[i] == B)
+                {
+                    a = A.Insert(i + 1,B.ToString());
+                }
+            }
+            Console.WriteLine("Результат: " + a);
+            Console.ReadKey();
+        }
+    }
+}
